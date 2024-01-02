cb01d0cceb17c94783cd6925cc743b010420270f
AlexandreLG2022 <a.legal.contact@gmail.com>
2024-01-02 15:11:27 +0100

    :bug: Fixed login button click problem

--- src/features/layout/auth/LoginButton.tsx
+++ src/features/layout/auth/LoginButton.tsx
@@ -1,15 +1,18 @@
 "use client";
 
 import {Button} from "@/components/ui/button";
+import { Loader } from "@/components/ui/loader";
 import {LogIn} from "lucide-react";
 import {signIn} from "next-auth/react";
+import { useTransition } from "react";
 
 export const LoginButton = () => {
+    const [isPending, startTransition] = useTransition()
     return (
-        <Button>
-            <LogIn className={'mr-2 h-4 w-4'} onClick={async () => {
-                await signIn()
-            }} />
+        <Button onClick={() => {
+            startTransition(() => signIn())
+        }}>
+            {isPending ? <Loader className="mr-2 h-4 w-4" /> : <LogIn className={'mr-2 h-4 w-4'}/>}
             Connexion
         </Button>
     )
