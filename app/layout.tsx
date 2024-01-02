3c48475f360dc333fc92406d39df6bf336a5a09f
AlexandreLG2022 <a.legal.contact@gmail.com>
2024-01-03 00:35:23 +0100

    :zap: Edited auth requirement message

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import {Header} from "@/src/features/layout/Header";
 import {Footer} from "@/src/features/layout/Footer";
 import {getAuthSession} from "@/lib/auth";
 import { ReduxStoreProvider } from '@/src/redux/ReduxStoreProvider';
+import {Info} from "lucide-react";
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -30,9 +31,12 @@ export default async function RootLayout({
                         <div className={'flex flex-col h-full'}>
                             <Header />
                             <div className="flex-1 max-w-xl m-auto py-6 w-full">
-                                {session?.user ? children : <h2 className={'text-xl font-bold'}>
-                                    Vous devez vous connecter pour utiliser l&apos;application
-                                </h2>}
+                                {session?.user ? children : <div className={'p-3 rounded-lg bg-blue-500 w-fit mx-auto'}>
+                                    <p className={'flex items-center justify-center text-lg font-medium text-white'}>
+                                        <Info size={20} className={'mr-2'} />
+                                        Vous devez vous connecter pour utiliser l&apos;application
+                                    </p>
+                                </div>}
                             </div>
                             <Footer />
                         </div>
