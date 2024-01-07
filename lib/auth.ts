402490a27b7c0c3e64565b9bc9595bc36630ee96
codebyalexx <a.legal.contact@gmail.com>
2024-01-07 19:31:43 +0100

    :zap: Added google sesion profile support

--- lib/auth.ts
+++ lib/auth.ts
@@ -5,18 +5,40 @@ import {AuthOptions, getServerSession} from "next-auth";
 import {PrismaAdapter} from "@next-auth/prisma-adapter";
 import {prisma} from "@/lib/prisma";
 
+const profileTempalte = (profile) => ({
+    id: profile.id.toString(),
+    username: profile.login,
+    name: profile.name,
+    email: profile.email,
+    image: profile.avatar_url
+})
+
 export const authOptions: AuthOptions = {
     adapter: PrismaAdapter(prisma),
     providers: [
         GitHubProvider({
             clientId: env.GITHUB_ID,
             clientSecret: env.GITHUB_SECRET,
+            profile: profileTempalte
         }),
         GoogleProvider({
             clientId: env.GOOGLE_CLIENT_ID,
-            clientSecret: env.GOOGLE_CLIENT_SECRET
+            clientSecret: env.GOOGLE_CLIENT_SECRET,
+            profile: (profile) => ({
+                id: profile.sub,
+                name: profile.name,
+                email: profile.email,
+                image: profile.picture
+            })
         })
     ],
+    callbacks: {
+        session({session, user}) {
+            if (!session.user) return session;
+            session.user.id = user.id;
+            return session;
+        }
+    },
     pages: {
         signIn: '/auth'
     }
