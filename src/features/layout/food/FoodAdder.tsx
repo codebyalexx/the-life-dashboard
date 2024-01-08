e028e4f58b68ad945b6ea0c1a445971abc90a75c
AlexandreLG2022 <a.legal.contact@gmail.com>
2024-01-08 23:47:58 +0100

    :bug: Fixed state update errors on FoodAdder

--- src/features/layout/food/FoodAdder.tsx
+++ src/features/layout/food/FoodAdder.tsx
@@ -9,14 +9,15 @@ import { addFood } from "@/src/actions/food.action"
 import { useToast } from "@/components/ui/use-toast"
 import { useDispatch } from "react-redux"
 import { addItem } from "./foodSlice"
+import { Label } from "@/components/ui/label"
 
 export const FoodAdder = ({session}: {session: Session | null}) => {
     const {toast} = useToast()
     const dispatch = useDispatch()
 
-    const defaultNutriments = {calories: undefined, carbs: undefined, fat: undefined, proteins: undefined}
+    const defaultNutriments = {calories: 0, carbs: 0, fat: 0, proteins: 0}
 
-    const [name, setName] = useState(undefined)
+    const [name, setName] = useState('')
     const [nutriments, setNutriments] = useState(defaultNutriments)
 
     const onNutrimentChange = (e: ChangeEvent) => {
@@ -32,45 +33,60 @@ export const FoodAdder = ({session}: {session: Session | null}) => {
     return (
         <form onSubmit={(e) => e.preventDefault()}>
             <h3 className="text-lg font-semibold mb-2">Ajouter un aliment</h3>
-            <div className="relative">
-                <span className="absolute top-0 left-0 bottom-0 h-full p-2 flex items-center justify-center">
-                    <Tag size={16} />
-                </span>
-                <Input placeholder="Nom de l'aliment (optionnel)" id="name" name="name" value={name} onChange={(e) => setName(e.target.value)} className="w-full pl-8" />
-            </div>
-            <div className="p-3">
+            <div>
+                <Label htmlFor="name" className="mb-1">Nom</Label>
                 <div className="relative">
                     <span className="absolute top-0 left-0 bottom-0 h-full p-2 flex items-center justify-center">
-                        <Calculator size={16} />
+                        <Tag size={16} />
                     </span>
-                    <Input placeholder="Calories" id="calories" name="calories" type="number" min={0} max={10000}  value={nutriments.calories} onChange={onNutrimentChange} className="w-full pl-8" />
+                    <Input placeholder="Nom (optionnel)" id="name" name="name" value={name} onChange={(e) => setName(e.target.value)} className="w-full pl-8" />
+                </div>
+            </div>
+            <div className="p-2">
+                <div>
+                    <Label htmlFor="calories" className="mb-1">Calories</Label>
+                    <div className="relative">
+                        <span className="absolute top-0 left-0 bottom-0 h-full p-2 flex items-center justify-center">
+                            <Calculator size={16} />
+                        </span>
+                        <Input placeholder="Calories" id="calories" name="calories" type="number" min={0} max={10000}  value={nutriments.calories} onChange={onNutrimentChange} className="w-full pl-8" />
+                    </div>
                 </div>
                 <div className="relative my-1">
                     <div aria-hidden="true" className="absolute inset-0 flex items-center">
                         <div className="w-full border-t border-muted-foreground" />
                     </div>
                     <div className="relative flex justify-center text-sm">
-                        <span className="px-2 bg-background text-muted-foreground">Ou</span>
+                        <span className="px-2 bg-background text-muted-foreground">Et / Ou</span>
                     </div>
                 </div>
                 <div className="grid grid-cols-3 gap-2 mb-2">
-                    <div className="relative">
-                        <span className="absolute top-0 left-0 bottom-0 h-full p-2 flex items-center justify-center">
-                            <Wheat size={16} />
-                        </span>
-                        <Input placeholder="Glucides" id="carbs" name="carbs" className="pl-8" type="number" min={0} max={1000} value={nutriments.carbs} onChange={onNutrimentChange} />
+                    <div>
+                        <Label htmlFor="carbs" className="mb-1">Glucides</Label>
+                        <div className="relative">
+                            <span className="absolute top-0 left-0 bottom-0 h-full p-2 flex items-center justify-center">
+                                <Wheat size={16} />
+                            </span>
+                            <Input placeholder="Glucides" id="carbs" name="carbs" className="pl-8" type="number" min={0} max={1000} value={nutriments.carbs} onChange={onNutrimentChange} />
+                        </div>
                     </div>
-                    <div className="relative">
-                        <span className="absolute top-0 left-0 bottom-0 h-full p-2 flex items-center justify-center">
-                            <EggFried size={16} />
-                        </span>
-                        <Input placeholder="Lipides" id="fat" name="fat" className="pl-8" type="number" min={0} max={1000} value={nutriments.fat} onChange={onNutrimentChange} />
+                    <div>
+                        <Label htmlFor="fat" className="mb-1">Lipides</Label>
+                        <div className="relative">
+                            <span className="absolute top-0 left-0 bottom-0 h-full p-2 flex items-center justify-center">
+                                <EggFried size={16} />
+                            </span>
+                            <Input placeholder="Lipides" id="fat" name="fat" className="pl-8" type="number" min={0} max={1000} value={nutriments.fat} onChange={onNutrimentChange} />
+                        </div>
                     </div>
-                    <div className="relative">
-                        <span className="absolute top-0 left-0 bottom-0 h-full p-2 flex items-center justify-center">
-                            <Beef size={16} />
-                        </span>
-                        <Input placeholder="Protéines" id="proteins" name="proteins" className="pl-8" type="number" min={0} max={1000} value={nutriments.proteins} onChange={onNutrimentChange} />
+                    <div>
+                        <Label htmlFor="proteins" className="mb-1">Protéines</Label>
+                        <div className="relative">
+                            <span className="absolute top-0 left-0 bottom-0 h-full p-2 flex items-center justify-center">
+                                <Beef size={16} />
+                            </span>
+                            <Input placeholder="Protéines" id="proteins" name="proteins" className="pl-8" type="number" min={0} max={1000} value={nutriments.proteins} onChange={onNutrimentChange} />
+                        </div>
                     </div>
                 </div>
             </div>
